d2cae6738d91e1eb63c5e910b3663b1aaa61e8bd
John Boehr <john@johnboehr.is>
2020-07-01 04:13:26 -0700

    Test against clang and move some nix files to a subdirectory

--- default.nix
+++ default.nix
@@ -1,16 +1,35 @@
 {
   pkgs ? import <nixpkgs> {},
+  stdenv ? pkgs.stdenv,
   php ? pkgs.php,
   buildPecl ? pkgs.callPackage <nixpkgs/pkgs/build-support/build-pecl.nix> {
-    inherit php;
+    inherit php stdenv;
+  },
+
+  gitignoreSource ? (import (pkgs.fetchFromGitHub {
+      owner = "hercules-ci";
+      repo = "gitignore";
+      rev = "00b237fb1813c48e20ee2021deb6f3f03843e9e4";
+      sha256 = "sha256:186pvp1y5fid8mm8c7ycjzwzhv7i6s3hh33rbi05ggrs7r3as3yy";
+  }) { inherit (pkgs) lib; }).gitignoreSource,
+
+  snowball-data ? pkgs.fetchFromGitHub {
+    owner = "snowballstem";
+    repo = "snowball-data";
+    rev = "8d35251e456139167a394ea315e4855247fa2471";
+    sha256 = "1jsdxc8ypfi3sbbmqln61zjlyvicx3vnd4c112dpmjcxl212yngw";
   },
 
   phpStemmerVersion ? null,
-  phpStemmerSrc ? ./.,
-  phpStemmerSha256 ? null
+  phpStemmerSha256 ? null,
+  phpStemmerSrc ? pkgs.lib.cleanSourceWith {
+    filter = (path: type: (builtins.all (x: x != baseNameOf path) [".idea" ".git" "ci.nix" ".travis.sh" ".travis.yml" ".ci" "nix" "default.nix"]));
+    src = gitignoreSource ./.;
+  }
 }:
 
-pkgs.callPackage ./derivation.nix {
-  inherit php buildPecl phpStemmerVersion phpStemmerSrc phpStemmerSha256;
+pkgs.callPackage ./nix/derivation.nix {
+  inherit stdenv php buildPecl;
+  inherit phpStemmerVersion phpStemmerSrc phpStemmerSha256;
+  inherit snowball-data;
 }
-
--- nix/derivation.nix
+++ nix/derivation.nix
@@ -1,9 +1,16 @@
-{ php, stdenv, autoreconfHook, fetchurl, libstemmer,
+{
+  lib, php, stdenv, autoreconfHook, fetchurl, libstemmer,
   buildPecl ? import <nixpkgs/pkgs/build-support/build-pecl.nix> {
     # re2c is required for nixpkgs master, must not be specified for <= 19.03
     inherit php stdenv autoreconfHook fetchurl;
   },
-  phpStemmerVersion ? null, phpStemmerSrc ? null, phpStemmerSha256 ? null }:
+  snowball-data ? null,
+  phpStemmerVersion ? null,
+  phpStemmerSrc ? null,
+  phpStemmerSha256 ? null,
+
+  checkSupport ? true
+}:
 
 let
   orDefault = x: y: (if (!isNull x) then x else y);
@@ -20,9 +27,15 @@ buildPecl rec {
 
   makeFlags = ["phpincludedir=$(out)/include/php/ext/stemmer"];
   buildInputs = [ libstemmer ];
+  nativeBuildInputs = []
+    ++ lib.optional checkSupport snowball-data
+    ;
 
-  doCheck = true;
+  preCheck = lib.optionalString checkSupport ''
+      php generate-tests.php ${snowball-data}
+    '';
+
+  doCheck = checkSupport;
   checkTarget = "test";
   checkFlags = ["REPORT_EXIT_STATUS=1" "NO_INTERACTION=1"];
 }
-
