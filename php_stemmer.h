f645a624833ee35e08d62fe4aa82b44fbb0d3b9c
John Boehr <jbboehr@mailbox.org>
2026-08-20 10:25:18 -0700

    Prepare release 2.0.0
    Mark the new major release after raising the supported PHP baseline to 8.1 and adding modern PIE packaging. Refresh the release date and copyright year exposed through phpinfo.

--- php_stemmer.h
+++ php_stemmer.h
@@ -1,10 +1,10 @@
 #ifndef PHP_STEMMER_PHP_H
 #define PHP_STEMMER_PHP_H 1
 
-#define PHP_STEMMER_VERSION "1.0.3"
-#define PHP_STEMMER_RELEASE "2024-06-19"
+#define PHP_STEMMER_VERSION "2.0.0"
+#define PHP_STEMMER_RELEASE "2026-08-20"
 #define PHP_STEMMER_LICENSE "BSD-3-Clause"
-#define PHP_STEMMER_AUTHORS "© anno Domini nostri Jesu Christi 2008-2024 Javeline B.V., John Boehr, & contributors"
+#define PHP_STEMMER_AUTHORS "© anno Domini nostri Jesu Christi 2008-2026 Javeline B.V., John Boehr, & contributors"
 #define PHP_STEMMER_EXTNAME "stemmer"
 
 extern zend_module_entry stemmer_module_entry;
